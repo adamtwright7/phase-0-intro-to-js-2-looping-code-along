45708c5d88ddd9c0733afc11840fa96e09b6cf32
Adam Wright <adamtwright7@gmail.com>
2022-10-26 23:32:44 -0400

    commit tree d2150a4fd6dd8bfeda5004ea8f9e383bf8000aaf

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(stringArray,eventName) {
+    let outArray = []
+    for (let i =0; i<stringArray.length; i+=1) {
+        outArray = [...outArray,
+        `Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`]
+    }
+    return outArray 
+}
+
+function countDown(number) {
+    let i2 = 0 
+    while (i2 <= number) {
+        console.log(number-i2)
+        i2 +=1
+    }
+}
